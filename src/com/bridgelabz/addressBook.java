87b78941a96f1023079440744e48ffcf83cad760
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-14 15:00:27 +0530

    Added:Edit the Existing Contact Details of AddressBook

--- src/com/bridgelabz/addressBook.java
+++ src/com/bridgelabz/addressBook.java
@@ -2,17 +2,17 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
-public class newAddressBook {
+public class addressBook {
 
     Scanner sc = new Scanner(System.in);
-
     /*
     Declaring The Add Contact Method
     And Entering The Contact Details By Using Scanner Class
     And Printing The Contact Details Of Person
      */
+    contactDetails person = new contactDetails();
     public void addContact() {
-        contactDetails person = new contactDetails();
+        //contactDetails person = new contactDetails();
         System.out.println("Enter First Name: ");
         String firstName = sc.nextLine();
         System.out.println("Enter last Name: ");
@@ -36,6 +36,23 @@ public class newAddressBook {
         person.setZip(zip.intValue());
         person.setPhoneNumber(phoneNumber.intValue());
         person.setEmail(email);
-        System.out.println("The Contact Details of "+firstName+ "\n"+ person);
+        System.out.println("The Contact Details of " + firstName + "\n" + person);
+    }
+    /*
+    Declaring The Edit Contact Method
+    TO Edit The Details Of Contact
+    The Details Of Contact Edit By Using FirstName
+    If First Name Is Match The Contact Will Edit
+    */
+    public void editContact() {
+        //Scanner editName = new Scanner(System.in);
+        System.out.println("Enter the firstName of person");
+        String editName = sc.nextLine();
+        if (editName.equalsIgnoreCase(person.getFirstName()))
+            addContact();
+        else
+            System.out.println("The Entered First Name Is Not Match");
+        editContact();
     }
 }
+
--- src/com/bridgelabz/addressBookMain.java
+++ src/com/bridgelabz/addressBookMain.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 /*
 Declaring Main method Here
 Defining newContact Object
@@ -7,8 +10,16 @@ And Printing The new Contact Details
 public class addressBookMain {
     public static void main(String[] args) {
         System.out.printf("Welcome to Address Book Program \n");
-        newAddressBook person = new newAddressBook();
+        Scanner sc = new Scanner(System.in);
+        addressBook person = new addressBook();
         person.addContact();//Calling Add Contacts Method
-
+        System.out.println("Enter Y To Edit The Contact");
+        String op = sc.nextLine();
+        //If Condition To Edit The Contact Details Of Person
+        if (op.equals("y") || op.equals("Y")) {
+             person.editContact(); //Calling The Edit Contact Method
+            System.out.println("You have Entered following data");
+            System.out.println("The Contact Details After Editing : " + person);
+        }
     }
 }
